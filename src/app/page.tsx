00608e83150a3a20595437811b2c04a2f1a330d7
gsngdavid <gsngdavid@gmail.com>
2024-01-12 08:01:41 +0200

    feat: add skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,92 @@ export default function Home() {
         </div>
 
         <div className="grid grid-cols-1  lg:grid-cols-3 gap-4">
-          {[1, 2, 3].map(index => <BigProject key={index} />)}
+          {[1, 2, 3].map((index) => (
+            <BigProject key={index} />
+          ))}
+        </div>
+      </div>
+
+      <div className="container my-20">
+        <div className="relative py-8">
+          <h1 className="text-2xl after:content-['']  after:absolute after:top-1/2 after:translate-x-2 after:inline-block after:w-1/3 after:h-0.5 after:bg-primary">
+            <span className="text-primary">#</span> Skills
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-3 lg:grid-cols-5 gap-4">
+          <div className="sm:hidden lg:col-span-2">
+            <Image
+              src="/images/icons.png"
+              alt="Icons"
+              width={200}
+              height={100}
+              className="w-full"
+            />
+          </div>
+
+          <div>
+            <div className="border border-gray">
+              <h1 className="border-b border-gray py-1 px-2">Languages</h1>
+              <div className="flex gap-2 flex-wrap text-sm text-gray py-1 px-2">
+                <span>TypeScript</span>
+                <span>Lua</span>
+                <span>Python</span>
+                <span>JavaScript</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <div className="border border-gray">
+              <h1 className="border-b border-gray py-1 px-2">Databases</h1>
+              <div className="flex gap-2 flex-wrap text-sm text-gray py-1 px-2">
+                <span>SQLite</span>
+                <span>PostgresSQL</span>
+                <span>Mongo</span>
+              </div>
+            </div>
+
+            <div className="border border-gray">
+              <h1 className="border-b border-gray py-1 px-2">Others</h1>
+              <div className="flex gap-2 flex-wrap text-sm text-gray py-1 px-2">
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>EJS</span>
+                <span>SCSS</span>
+                <span>REST</span>
+                <span>Jinja</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <div className="border border-gray">
+              <h1 className="border-b border-gray py-1 px-2">Tools</h1>
+              <div className="flex gap-2 flex-wrap text-sm text-gray py-1 px-2">
+                <span>VSCode</span>
+                <span>Neovim</span>
+                <span>Linux</span>
+                <span>Figma</span>
+                <span>XFCE</span>
+                <span>Arch</span>
+                <span>Git</span>
+                <span>Font Awesome</span>
+              </div>
+            </div>
+
+            <div className="border border-gray">
+              <h1 className="border-b border-gray py-1 px-2">Frameworks</h1>
+              <div className="flex gap-2 flex-wrap text-sm text-gray py-1 px-2">
+                <span>Vue</span>
+                <span>React</span>
+                <span>Disnake</span>
+                <span>Discord.js</span>
+                <span>Flask</span>
+                <span>Express</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
