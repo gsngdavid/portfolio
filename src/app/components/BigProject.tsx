b1117098a3175ac3398cac5fc9557726a9ce79a4
gsngdavid <gsngdavid@gmail.com>
2024-01-16 08:09:27 +0200

    feat: create ui folder

--- src/app/components/BigProject.tsx
+++ src/app/components/BigProject.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Button from "./Button";
+import Button from "./ui/Button";
 import { ButtonVariant } from "../types/enums";
 
 function BigProject() {
--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
-import { ButtonVariant } from "../types/enums";
+import { ButtonVariant } from "../../types/enums";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
